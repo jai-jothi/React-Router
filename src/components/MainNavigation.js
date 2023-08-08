e4431336ec8952c043adcdf9f5997716e8e20ca0
JAIJOTHI K <136154555+jai-jothi@users.noreply.github.com>
2023-08-08 14:31:15 +0530

    Add files via upload

---
+++ src/components/MainNavigation.js
@@ -0,0 +1,35 @@
+import classes from './MainNavigation.module.css';
+import { NavLink} from 'react-router-dom';
+import NewsLetterSignup from './NewsLettersignup';
+function MainNavigation() {
+  return (
+    <header className={classes.header}>
+      <nav>
+        <ul className={classes.list}>
+          <li>
+           <NavLink to="/" className={({isActive}) =>
+        isActive? classes.active:undefined} end>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/events" className={({isActive}) =>
+        isActive? classes.active:undefined}>Events</NavLink>
+          </li>
+          <li>
+          <NavLink
+              to="/newsletter"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Newsletter
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+     
+      <NewsLetterSignup />
+      </header>
+  );
+}
+
+export default MainNavigation;
